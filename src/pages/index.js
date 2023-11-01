197d3e069c0ece3d9c3f5a3941b4625fb364f82e
Michael Gruen <michael.gruen9@gmail.com>
2023-11-01 02:31:21 -0400

    src/pages/index.js - commit created by https://agenthub.dev/pipeline?run_id=G8TfEjpPRbyarMVvzXLJvy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
+```jsx
+// Importing required dependencies and components
 import React from 'react';
 import Sketchpad from '../components/Sketchpad';
 import DoodleDisplay from '../components/DoodleDisplay';
 import ErrorDisplay from '../components/ErrorDisplay';
 import { makeStyles } from '@material-ui/core/styles';
 
+// Setting up style rules for the Home component
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
@@ -15,14 +18,18 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+// Main functional component for home page
 export default function Home() {
+  // Applying style rules
   const classes = useStyles();
 
+  // Rendering the Home component that includes Sketchpad, DoodleDisplay, and ErrorDisplay 
   return (
     <div className={classes.root}>
-      <Sketchpad />
-      <DoodleDisplay />
-      <ErrorDisplay />
+      <Sketchpad /> {/* This section is for the Sketchpad component */}
+      <DoodleDisplay /> {/* This section is for the DoodleDisplay component */}
+      <ErrorDisplay /> {/* This section is for handling and displaying errors */}
     </div>
   );
 }
+```
